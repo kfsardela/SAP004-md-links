09ad4cfd2e29126f852bd5f26df2c5a515ca3178
Karine Sardela <kfsardela@gmail.com>
2020-07-08 20:05:30 -0300

    pegando apenas os arquivos Markdown

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-// const fs = require('fs')
+const path = require("path")
+const fs = require("fs")
 
-// const mdLinks = (path) => {
 
-
-// };
-
-// module.exports = mdLinks;
-
-fs = require('fs')
-fs.readFile('README.md', 'utf8', function(err, data) {
+const directoryPath = path.join(__dirname, "files")
+fs.readdir(directoryPath, function(err, files) {
     if (err) {
-        return console.log(err);
+        console.log("Error getting directory information.")
+    } else {
+        const filesMd = files.filter(function(file) {
+            return path.extname(file) == ".md"
+        })
+        filesMd.forEach(function(file) {
+            console.log(file)
+            fs.readFile(directoryPath + "/" + file, 'utf8', function(err, data) {
+                if (err) {
+                    return console.log(err);
+                } else {
+                    const regex = data.match(/(\[.*\])(\(.*\))/gim);
+                    regex.map((el) => {
+                        const linkText = el.split('](');
+                        const text = linkText[0].replace('[', '');
+                        const href = linkText[1].replace(')', '');
+                        return console.log({ file, text, href })
+
+                    });
+                }
+
+            });
+        })
+
+
     }
-    console.log(data);
-});
+})
